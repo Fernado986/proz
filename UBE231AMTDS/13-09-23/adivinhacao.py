9aa6fc5768de33c566ff64c44857c8b489c701e5
Lenício JR <lenicio.junior@hotmail.com>
2023-09-13 13:30:23 -0300

    Inserção de comentários

--- UBE231AMTDS/13-09-23/adivinhacao.py
+++ UBE231AMTDS/13-09-23/adivinhacao.py
@@ -1,3 +1,15 @@
+"""
+Este código é um simples jogo de adivinhação. Ele seleciona aleatoriamente um número entre 1 e 10, 
+e em seguida, permite ao usuário fazer três tentativas para adivinhar o número.
+A cada tentativa, o código verifica se o chute do usuário está correto:
+
+Se estiver correto, exibe uma mensagem de felicitação ("Parabéns, você acertou!") e encerra o jogo.
+
+Se estiver errado, informa ao usuário para tentar novamente e fornece mais uma chance para adivinhar.
+
+Após o usuário ter feito três tentativas ou adivinhado o número corretamente, o programa revela qual era o número correto.
+"""
+
 import random
 
 numero = random.randint(1, 10)
