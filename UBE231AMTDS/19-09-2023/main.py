2bace4ab60404ecbad220c98651c8051c3f3396e
Lenício JR <lenicio.junior@hotmail.com>
2023-09-19 08:26:14 -0300

    Aula 19-09-2023

---
+++ UBE231AMTDS/19-09-2023/main.py
@@ -0,0 +1,12 @@
+# append()  -   Adiciona elementos a uma lista
+# len()     -   Retorna a quantidade de elementos em uma lista
+# sum()     -   Realiza uma soma de todos os valores dentro da lista
+
+notas = [10, 8, 5, 9]
+
+notas.append(7)
+
+
+for i in (range(len(notas))):
+    print(notas[i])
+
